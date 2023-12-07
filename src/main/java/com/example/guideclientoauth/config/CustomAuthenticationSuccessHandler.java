43eddca400246ff357e428a279865da9448e8e68
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-07 13:02:08 +0300

    Added providers logic

--- src/main/java/com/example/guideclientoauth/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/guideclientoauth/config/CustomAuthenticationSuccessHandler.java
@@ -1,18 +1,31 @@
 package com.example.guideclientoauth.config;
 
+import com.project.core.commands.user.BindProviderIdToUserCommand;
+import com.project.core.commands.user.CreateUserFromProviderIdCommand;
+import com.project.core.commands.user.CreateUserProfileCommand;
+import com.project.core.queries.user.CheckUserProfileByProviderIdQuery;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class CustomAuthenticationSuccessHandler extends OncePerRequestFilter {
+    private final CommandGateway commandGateway;
+    private final QueryGateway queryGateway;
+
+    public CustomAuthenticationSuccessHandler(CommandGateway commandGateway, QueryGateway queryGateway) {
+        this.commandGateway = commandGateway;
+        this.queryGateway = queryGateway;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -22,8 +35,32 @@ public class CustomAuthenticationSuccessHandler extends OncePerRequestFilter {
             
             String provider = oauthToken.getAuthorizedClientRegistrationId();
 
+            var query = CheckUserProfileByProviderIdQuery.builder()
+                    .providerId(oauthToken.getName())
+                    .providerType(provider)
+                    .build();
+            var userProfileId = queryGateway.query(query, String.class).join();
+            if(userProfileId != null) {
+                var command = BindProviderIdToUserCommand.builder()
+                        .userId(userProfileId)
+                        .providerId(oauthToken.getName())
+                        .providerType(provider)
+                        .build();
+                commandGateway.send(command);
+                filterChain.doFilter(request, response);
+                return;
+            }
+
             if ("github".equals(provider)) {
+                var command = CreateUserFromProviderIdCommand.builder()
+                        .userId(UUID.randomUUID().toString())
+                        .userName(oauthToken.getName())
+                        .providerId(oauthToken.getName())
+                        .providerType(provider)
+                        .build();
+                commandGateway.send(command);
                 System.out.println("GitHub authentication: " + authentication.getName());
+
             } else if ("google".equals(provider)) {
                 System.out.println("Google authentication: " + authentication.getName());
             }
