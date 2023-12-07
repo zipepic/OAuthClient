733a8290414cdb7a198e5da2953f339e2bafea62
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-07 12:58:29 +0300

    Added new trusted classes

--- src/main/java/com/example/guideclientoauth/GuideClientOauthApplication.java
+++ src/main/java/com/example/guideclientoauth/GuideClientOauthApplication.java
@@ -1,7 +1,11 @@
 package com.example.guideclientoauth;
 
+import com.project.core.commands.user.*;
+import com.project.core.queries.user.CheckUserProfileByProviderIdQuery;
+import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GuideClientOauthApplication {
@@ -9,5 +13,21 @@ public class GuideClientOauthApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuideClientOauthApplication.class, args);
     }
+    @Bean
+    public XStream xStream() {
+        XStream xStream = new XStream();
+        registerClasses(xStream,
+                CreateUserProfileCommand.class,
+                CheckUserProfileByProviderIdQuery.class,
+                BindProviderIdToUserCommand.class,
+                CreateUserFromProviderIdCommand.class);
 
+        return xStream;
+    }
+
+    private void registerClasses(XStream xStream, Class<?>... classes) {
+        for (Class<?> clazz : classes) {
+            xStream.allowTypeHierarchy(clazz);
+        }
+    }
 }
