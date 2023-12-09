5f9c1dd1de91fe2944b4682da6e232e66fe23ffe
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-09 23:40:51 +0300

    Added repository in configuration filter's

--- src/main/java/com/example/guideclientoauth/config/SecurityConfig.java
+++ src/main/java/com/example/guideclientoauth/config/SecurityConfig.java
@@ -1,36 +1,54 @@
 package com.example.guideclientoauth.config;
 
+import com.example.guideclientoauth.query.api.data.UserProfileProviderMappingLookUpRepository;
 import jakarta.servlet.Filter;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     private final QueryGateway queryGateway;
     private final CommandGateway commandGateway;
+    private final UserProfileProviderMappingLookUpRepository userProfileProviderMappingLookUpRepository;
     @Autowired
-    public SecurityConfig(QueryGateway queryGateway, CommandGateway commandGateway) {
+    public SecurityConfig(QueryGateway queryGateway, CommandGateway commandGateway, UserProfileProviderMappingLookUpRepository userProfileProviderMappingLookUpRepository) {
         this.queryGateway = queryGateway;
         this.commandGateway = commandGateway;
+        this.userProfileProviderMappingLookUpRepository = userProfileProviderMappingLookUpRepository;
     }
-
+//    @Bean
+//    @Order(1)
+//    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .securityMatcher(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+//                .authorizeHttpRequests( auth -> {
+//                    auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll();
+//                })
+//                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
+//                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+//                .build();
+//    }
     @Bean
+    @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         return http
                 .authorizeHttpRequests(request->{
                     request.requestMatchers("/").permitAll();
                     request.anyRequest().authenticated();
                         })
+
                 .oauth2Login(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .addFilterAfter(customAuthenticationSuccessHandler(), OAuth2LoginAuthenticationFilter.class)
@@ -38,6 +56,6 @@ public class SecurityConfig {
     }
     @Bean
     public Filter customAuthenticationSuccessHandler() {
-        return new CustomAuthenticationSuccessHandler(commandGateway, queryGateway);
+        return new CustomAuthenticationSuccessHandler(commandGateway, queryGateway, userProfileProviderMappingLookUpRepository);
     }
 }
