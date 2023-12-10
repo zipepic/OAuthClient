6bb795c6d1a47a044dfdaf77c070aee15e6e4798
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-10 23:38:17 +0300

    Added trusted classes

--- src/main/java/com/example/guideclientoauth/GuideClientOauthApplication.java
+++ src/main/java/com/example/guideclientoauth/GuideClientOauthApplication.java
@@ -2,8 +2,10 @@ package com.example.guideclientoauth;
 
 import com.project.core.commands.user.*;
 import com.project.core.dto.TokenAuthorizationCodeDTO;
+import com.project.core.events.app.ApplicationCreatedEvent;
 import com.project.core.events.user.UserProfileCreatedEvent;
 import com.project.core.events.user.UserProfileProviderMappingLookUpCreatedEvent;
+import com.project.core.events.user.UserWereCompletedEvent;
 import com.project.core.queries.user.CheckUserProfileByProviderIdQuery;
 import com.thoughtworks.xstream.XStream;
 import org.springframework.boot.SpringApplication;
@@ -20,23 +22,29 @@ public class GuideClientOauthApplication {
     @Bean
     public XStream xStream() {
         XStream xStream = new XStream();
-        registerClasses(xStream,
-                CreateUserProfileCommand.class,
-                CheckUserProfileByProviderIdQuery.class,
-                BindProviderIdToUserCommand.class,
-                CreateUserFromProviderIdCommand.class,
-                GenerateTokenByProviderIdCommand.class,
-                TokenAuthorizationCodeDTO.class,
-                UserProfileProviderMappingLookUpCreatedEvent.class,
-                UserProfileCreatedEvent.class,
-                AuthProvider.class);
+//        registerClasses(xStream,
+//                CreateUserProfileCommand.class,
+//                CheckUserProfileByProviderIdQuery.class,
+//                BindProviderIdToUserCommand.class,
+//                CreateUserFromProviderIdCommand.class,
+//                GenerateTokenByProviderIdCommand.class,
+//                TokenAuthorizationCodeDTO.class,
+//                UserProfileProviderMappingLookUpCreatedEvent.class,
+//                UserProfileCreatedEvent.class,
+//                AuthProvider.class,
+//                UserWereCompletedEvent.class,
+//                ApplicationCreatedEvent.class);
+
+        xStream.allowTypesByWildcard(new String[] {
+                "com.project.core.**"
+        });
 
         return xStream;
     }
 
-    private void registerClasses(XStream xStream, Class<?>... classes) {
-        for (Class<?> clazz : classes) {
-            xStream.allowTypeHierarchy(clazz);
-        }
-    }
+//    private void registerClasses(XStream xStream, Class<?>... classes) {
+//        for (Class<?> clazz : classes) {
+//            xStream.allowTypeHierarchy(clazz);
+//        }
+//    }
 }
