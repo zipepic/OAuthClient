69c10bd67761c7215c67db41b6157b26e3be0f16
xzz1p <olezhka.kirilov.97@mail.ru>
2023-12-10 23:51:42 +0300

    Refactor UserProfileEventHandler in guideclientoauth for UserWereCompletedEvent

--- src/main/java/com/example/guideclientoauth/query/api/event/UserProfileEventHandler.java
+++ src/main/java/com/example/guideclientoauth/query/api/event/UserProfileEventHandler.java
@@ -3,6 +3,7 @@ package com.example.guideclientoauth.query.api.event;
 import com.example.guideclientoauth.query.api.data.UserProfileProviderMappingLookUpEntity;
 import com.example.guideclientoauth.query.api.data.UserProfileProviderMappingLookUpRepository;
 import com.project.core.events.user.UserProfileProviderMappingLookUpCreatedEvent;
+import com.project.core.events.user.UserWereCompletedEvent;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,15 +17,27 @@ public class UserProfileEventHandler {
         this.userProfileProviderMappingLookUpRepository = userProfileProviderMappingLookUpRepository;
     }
 
+//    @EventHandler
+//     public void on(UserProfileProviderMappingLookUpCreatedEvent event) {
+//        var entity = new UserProfileProviderMappingLookUpEntity();
+//        if(event.getAuthProvider() == AuthProvider.GITHUB){
+//            entity.setGithubId(event.getProviderId());
+//        }else if(event.getAuthProvider() == AuthProvider.GOOGLE){
+//            entity.setGoogleId(event.getProviderId());
+//        }
+//        System.out.println("event.getProviderId() = " + event.getProviderId());
+//        userProfileProviderMappingLookUpRepository.save(entity);
+//     }
     @EventHandler
-     public void on(UserProfileProviderMappingLookUpCreatedEvent event) {
+    public void handle(UserWereCompletedEvent event){
         var entity = new UserProfileProviderMappingLookUpEntity();
+        entity.setUserId(event.getUserId());
         if(event.getAuthProvider() == AuthProvider.GITHUB){
             entity.setGithubId(event.getProviderId());
         }else if(event.getAuthProvider() == AuthProvider.GOOGLE){
             entity.setGoogleId(event.getProviderId());
         }
-        System.out.println("event.getProviderId() = " + event.getProviderId());
         userProfileProviderMappingLookUpRepository.save(entity);
-     }
+        System.out.println("event.getProviderId() = " + event.getProviderId());
+    }
 }
